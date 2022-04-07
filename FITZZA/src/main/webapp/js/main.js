82e8da5fb6e0af2ec68f6263914c57521b8c346a
SoobeenMun <96978871+NYANGSoobeen@users.noreply.github.com>
2022-04-07 13:14:57 +0900

    webapp 커밋
    webapp 업로드

--- FITZZA/src/main/webapp/js/main.js
+++ FITZZA/src/main/webapp/js/main.js
@@ -1,3 +1,4 @@
+/*토글 메뉴*/
 $(document).ready(function(){
     $('#menu_bar').on('click',function(){
         $('#gnb').removeClass('hide').addClass('show');
@@ -10,7 +11,8 @@ $(document).ready(function(){
     }); 
 });
 
-var html = $("html");
+/*스크롤 무빙*/
+/* var html = $("html");
 var page = 1;
 html.animate({scrollTop:0}, 10);
 $(window).on("wheel", function(e) {
@@ -25,8 +27,9 @@ $(window).on("wheel", function(e) {
     // var posTop =(page-1) * $(window).height();
     var posTop =(page-1) * 1080;
     html.animate({scrollTop : posTop});
-});
+}); */
 
+/*style_reco*/
 $(document).ready(function(){
 	$('#person1').on('click',function(){
 		$('#person1').css({
@@ -82,3 +85,153 @@ $(document).ready(function(){
 		}	
 	});
 });
+/*review*/
+
+            $(document).ready(function ($) {
+
+                "use strict";
+
+                //Page loader
+                if ($('.pageloader').length) {
+
+                    $('.pageloader').toggleClass('is-active');
+
+                    $(window).on('load', function () {
+                        var pageloaderTimeout = setTimeout(function () {
+                            $('.pageloader').toggleClass('is-active');
+                            $('.infraloader').toggleClass('is-active')
+                            clearTimeout(pageloaderTimeout);
+                        }, 700);
+                    })
+                }
+
+               
+                //Index hero animated header
+                if ($('#large-header').length) {
+                    // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+                    // http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+
+                    // requestAnimationFrame polyfill by Erik MÃ¶ller. fixes from Paul Irish and Tino Zijdel
+
+                    // MIT license
+
+                    (function () {
+                        var lastTime = 0;
+                        var vendors = ['ms', 'moz', 'webkit', 'o'];
+                        for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+                            window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+                            window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame']
+                            || window[vendors[x] + 'CancelRequestAnimationFrame'];
+                        }
+
+                        if (!window.requestAnimationFrame)
+                            window.requestAnimationFrame = function (callback, element) {
+                                var currTime = new Date().getTime();
+                                var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+                                var id = window.setTimeout(function () { callback(currTime + timeToCall); },
+                                                           timeToCall);
+                                lastTime = currTime + timeToCall;
+                                return id;
+                            };
+
+                        if (!window.cancelAnimationFrame)
+                            window.cancelAnimationFrame = function (id) {
+                                clearTimeout(id);
+                            };
+                    }());
+
+                    (function () {
+
+                        var width, height, largeHeader, canvas, ctx, circles, target, animateHeader = true;
+
+                        // Main
+                        initHeader();
+                        addListeners();
+
+                        function initHeader() {
+                            width = window.innerWidth;
+                            height = window.innerHeight;
+                            target = { x: 0, y: height };
+
+                            largeHeader = document.getElementById('large-header');
+                            largeHeader.style.height = height + 'px';
+
+                            canvas = document.getElementById('demo-canvas');
+                            canvas.width = width;
+                            canvas.height = height;
+                            ctx = canvas.getContext('2d');
+
+                            // create particles
+                            circles = [];
+                            for (var x = 0; x < width * 0.5; x++) {
+                                var c = new Circle();
+                                circles.push(c);
+                            }
+                            animate();
+                        }
+
+                        // Event handling
+                        function addListeners() {
+                            window.addEventListener('scroll', scrollCheck);
+                            window.addEventListener('resize', resize);
+                        }
+
+                        function scrollCheck() {
+                            if (document.body.scrollTop > height) animateHeader = false;
+                            else animateHeader = true;
+                        }
+
+                        function resize() {
+                            width = window.innerWidth;
+                            height = window.innerHeight;
+                            largeHeader.style.height = height + 'px';
+                            canvas.width = width;
+                            canvas.height = height;
+                        }
+
+                        function animate() {
+                            if (animateHeader) {
+                                ctx.clearRect(0, 0, width, height);
+                                for (var i in circles) {
+                                    circles[i].draw();
+                                }
+                            }
+                            requestAnimationFrame(animate);
+                        }
+
+                        // Canvas manipulation
+                        function Circle() {
+                            var _this = this;
+
+                            // constructor
+                            (function () {
+                                _this.pos = {};
+                                init();
+                                console.log(_this);
+                            })();
+
+                            function init() {
+                                _this.pos.x = Math.random() * width;
+                                _this.pos.y = height + Math.random() * 200;
+                                _this.alpha = 0.1 + Math.random() * 0.3;
+                                _this.scale = 0.3 + Math.random() * 0.3;
+                                _this.velocity = Math.random();
+                            }
+
+                            this.draw = function () {
+                                if (_this.alpha <= 0) {
+                                    init();
+                                }
+                                _this.pos.y -= _this.velocity;
+                                _this.alpha -= 0.0005;
+                                ctx.beginPath();
+                                ctx.arc(_this.pos.x, _this.pos.y, _this.scale * 15, 0, 2 * Math.PI, false);
+                                ctx.fillStyle = 'rgba(255,255,255,' + _this.alpha + ')';
+                                ctx.fill();
+                            };
+                        }
+
+                    })();
+                }
+
+            })
