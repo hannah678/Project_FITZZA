a083d90a80d91ded6057a3559305b19e455196ea
Hyeyoung Kim <hyeyoung.dev@gmail.com>
2022-04-20 14:01:55 +0900

    ID초기값

--- 세미프로젝트 DB초기값.sql
+++ 세미프로젝트 DB초기값.sql
@@ -11,19 +11,38 @@ select * from board_old;
 select * from board_type;
 select * from board_common_image;
 select * from gender;
-select * from trade_detail;
+select * from like_clicker;
 
-delete from user where user_id='aaaa';
-delete from board where type_num=3;
-
-insert into user(user_id, user_pwd, user_name, user_nickname, email, tel, exp, profile_image, permission)
-values('aaaa', '1111', '곽덕수', '인사동옹기장인', 'moneymoney@naver.com', '010-1111-2222', 150, 'profileDefault.png', 'user');
-insert into user(user_id, user_pwd, user_name, user_nickname, email, tel, exp, profile_image, permission)
-values('bbbb', '1111', '고구마', '고구마조아', 'goguma@naver.com', '010-0000-2222', 350, 'profileDefault.png', 'user');
-insert into user(user_id, user_pwd, user_name, user_nickname, email, tel, exp, profile_image, permission)
-values('cccc', '1111', '홍길동', '홍홍홍홍홍', 'hong@naver.com', '010-1111-3333', 0, 'profileDefault.png', 'user');
 insert into user(user_id, user_pwd, user_name, user_nickname, email, tel, exp, profile_image, permission)
 values('admin', '1111', '관리자', '관리자', 'admin@naver.com', '010-0000-0000', 1000, 'adminImg.jpg', 'admin');
+insert into user(user_id, user_pwd, user_name, user_nickname, email, tel, exp, profile_image, permission)
+values('aaaa', '1111', '곽덕수', '인사동옹기장인', 'moneymoney@naver.com', '010-1111-2222',350, 'profileDefault.png', 'user');
+insert into user(user_id, user_pwd, user_name, user_nickname, email, tel, exp, profile_image, permission)
+values('bbbb', '1111', '고구마', '고구마조아', 'goguma@naver.com', '010-0000-2222',100, 'profileDefault.png', 'user');
+insert into user(user_id, user_pwd, user_name, user_nickname, email, tel, exp, profile_image, permission)
+values('cccc', '1111', '김고구마', '구우운고구마', 'goguma12@naver.com', '010-3434-2222', 0, 'profileDefault.png', 'user');
+insert into user(user_id, user_pwd, user_name, user_nickname, email, tel, exp, profile_image, permission)
+values('dddd', '1111', '홍길동', '홍홍홍홍홍', 'hong01@naver.com', '010-2344-3333', 0, 'profileDefault.png', 'user');
+insert into user(user_id, user_pwd, user_name, user_nickname, email, tel, exp, profile_image, permission)
+values('eeee', '1111', '김길동', '길동길동이', 'hong02@naver.com', '010-4534-5553', 0, 'profileDefault.png', 'user');
+insert into user(user_id, user_pwd, user_name, user_nickname, email, tel, exp, profile_image, permission)
+values('ffff', '1111', '이길동', '포인트사냥꾼', 'hong03@naver.com', '010-1111-3663', 0, 'profileDefault.png', 'user');
+insert into user(user_id, user_pwd, user_name, user_nickname, email, tel, exp, profile_image, permission)
+values('gggg', '1111', '세종대왕', '한글지키미', 'hong04@naver.com', '010-7771-3333', 0, 'profileDefault.png', 'user');
+insert into user(user_id, user_pwd, user_name, user_nickname, email, tel, exp, profile_image, permission)
+values('hhhh', '1111', '이순신', '뉴비뉴비', 'hong05@naver.com', '010-1888-3333', 0, 'profileDefault.png', 'user');
+insert into user(user_id, user_pwd, user_name, user_nickname, email, tel, exp, profile_image, permission)
+values('iiii', '1111', '강감찬', '데님쟁이', 'hong06@naver.com', '010-1999-3333', 0, 'profileDefault.png', 'user');
+insert into user(user_id, user_pwd, user_name, user_nickname, email, tel, exp, profile_image, permission)
+values('jjjj', '1111', '고길동', '릘릐릐야', 'hong07@naver.com', '010-1344-3333', 0, 'profileDefault.png', 'user');
+insert into user(user_id, user_pwd, user_name, user_nickname, email, tel, exp, profile_image, permission)
+values('kkkk', '1111', '박길동', '보라돌잉', 'hong08@naver.com', '010-1999-3333', 0, 'profileDefault.png', 'user');
+insert into user(user_id, user_pwd, user_name, user_nickname, email, tel, exp, profile_image, permission)
+values('ㅣㅣㅣㅣ', '1111', '장길동', '숨쉬는NPC', 'hong09@naver.com', '010-3331-3453', 0, 'profileDefault.png', 'user');
+insert into user(user_id, user_pwd, user_name, user_nickname, email, tel, exp, profile_image, permission)
+values('mmmm', '1111', '심길동', 'o_o_vovo', 'hong10@naver.com', '010-3331-1223', 0, 'profileDefault.png', 'user');
+insert into user(user_id, user_pwd, user_name, user_nickname, email, tel, exp, profile_image, permission)
+values('nnnn', '1111', '백길동', '개미파워a', 'hong11@naver.com', '010-3351-3453', 0, 'profileDefault.png', 'user');
 
 insert into board_type (type_num, type_name) values(1, '오늘의코디');
 insert into board_type (type_num, type_name) values(2, '스타일추천');
@@ -51,46 +70,16 @@ insert into user_grade(grade, exp_min, exp_max, frame_img) values(3, 200, 299, '
 insert into user_grade(grade, exp_min, exp_max, frame_img) values(4, 300, 399, '/img/level4.png');
 insert into user_grade(grade, exp_min, exp_max, frame_img) values(5, 400, 1000000, '/img/level5.png');
 -- 사진 안넣어서 안 됨
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '111옷 리뷰', '이 옷 좋음','W','aaaa','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '222옷 리뷰', '이 옷 별루','M','bbbb','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '333옷 리뷰', '이 옷 좋음','U','cccc','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '444옷 리뷰', '이 옷 별루','W','aaaa','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '555옷 리뷰', '이 옷 좋음','M','bbbb','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '666옷 리뷰', '이 옷 별루','U','cccc','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '777옷 리뷰', '이 옷 좋음','W','aaaa','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '888옷 리뷰', '이 옷 별루','M','bbbb','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '999옷 리뷰', '이 옷 좋음','W','cccc','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '1010옷 리뷰', '이 옷 별루','U','aaaa','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '1111옷 리뷰', '이 옷 좋음','W','bbbb','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '1212옷 리뷰', '이 옷 별루','M','cccc','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '1313옷 리뷰', '이 옷 좋음','U','aaaa','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '1414옷 리뷰', '이 옷 별루','W','bbbb','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '1515옷 리뷰', '이 옷 좋음','M','cccc','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '1616옷 리뷰', '이 옷 별루','U','aaaa','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '1717옷 리뷰', '이 옷 별루','W','bbbb','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '1818옷 리뷰', '이 옷 좋음','M','cccc','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '1919옷 리뷰', '이 옷 별루','U','aaaa','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '2020옷 리뷰', '이 옷 별루','W','bbbb','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '2121옷 리뷰', '이 옷 좋음','M','cccc','0:0:0:0:0:0:0:1');
-insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '2222옷 리뷰', '이 옷 별루','U','aaaa','0:0:0:0:0:0:0:1');
--- 파일명 중복 안됨
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(1, 'codi_book_img1.png', 'codi_book_img2(2).png', 'codi_book_img3(2).png', 'codi_book_img4(2).png', 'codi_book_img5(2).png');
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(2, 'codi_book_img2.png(1)', 'codi_book_img2(3).png', 'codi_book_img3(3).png', 'codi_book_img4(3).png', 'codi_book_img5(3).png');
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(3, 'codi_book_img3.png', 'codi_book_img2.png(4)', 'codi_book_img3(4).png', 'codi_book_img4(4).png', 'codi_book_img5(4).png');
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(4, 'codi_book_img4.png', 'codi_book_img2.png(5)', 'codi_book_img3(5).png', 'codi_book_img4(5).png', 'codi_book_img5(5).png');
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(5, 'codi_book_img5.png', 'codi_book_img2.png(6)', 'codi_book_img3(6).png', 'codi_book_img4(6).png', 'codi_book_img5(6).png');
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(6, 'codi_book_img6.png', 'codi_book_img2.png(7)', 'codi_book_img3(7).png', 'codi_book_img4(7).png', 'codi_book_img5(7).png');
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(7, 'codi_book_img7.png', 'codi_book_img2.png(8)', 'codi_book_img3(8).png', 'codi_book_img4(8).png', 'codi_book_img5(8).png');
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(8, 'codi_book_img8.png', 'codi_book_img2.png', 'codi_book_img3.png', 'codi_book_img4.png', 'codi_book_img5.png');
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(9, 'codi_book_img9.png', 'codi_book_img2.png', 'codi_book_img3.png', 'codi_book_img4.png', 'codi_book_img5.png');
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(10, 'codi_book_img10.png', 'codi_book_img2.png', 'codi_book_img3.png', 'codi_book_img4.png', 'codi_book_img5.png');
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(11, 'codi_book_img11.png', 'codi_book_img2.png', 'codi_book_img3.png', 'codi_book_img4.png', 'codi_book_img5.png');
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(12, 'codi_book_img12.png', 'codi_book_img2.png', 'codi_book_img3.png', 'codi_book_img4.png', 'codi_book_img5.png');
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(13, 'codi_book_img1(1).png', 'codi_book_img2.png', 'codi_book_img3.png', 'codi_book_img4.png', 'codi_book_img5.png');
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(14, 'codi_book_img2(1).png', 'codi_book_img2.png', 'codi_book_img3.png', 'codi_book_img4.png', 'codi_book_img5.png');
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(15, 'codi_book_img3(1).png', 'codi_book_img2.png', 'codi_book_img3.png', 'codi_book_img4.png', 'codi_book_img5.png');
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(16, 'codi_book_img4(1).png', 'codi_book_img2.png', 'codi_book_img3.png', 'codi_book_img4.png', 'codi_book_img5.png');
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(17, 'codi_book_img5(1).png', 'codi_book_img2.png', 'codi_book_img3.png', 'codi_book_img4.png', 'codi_book_img5.png');
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(18, 'codi_book_img6(1).png', 'codi_book_img2.png', 'codi_book_img3.png', 'codi_book_img4.png', 'codi_book_img5.png');
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(19, 'codi_book_img7(1).png', 'codi_book_img2.png', 'codi_book_img3.png', 'codi_book_img4.png', 'codi_book_img5.png');
-insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(20, 'codi_book_img8(1).png', 'codi_book_img2.png', 'codi_book_img3.png', 'codi_book_img4.png', 'codi_book_img5.png');
+-- 구매후기 게시판
+insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '[신발] 아앨랜드 슲리퍼', '아직은 개시 못했음','U','aaaa','0:0:0:0:0:0:0:1');
+insert into board(type_num, title, content, gender_type, user_id, ip) values (4, '애플워치 까르띠에 스트랩', '넘 예쁜데 갤럭시를 쓰는 나..ㅋㅋ','U','bbbb','0:0:0:0:0:0:0:1');
+
+-- 파일명 중복 안됨(upload에 이미지파일 넣기
+-- 구매후기 게시판 이미지
+insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(1, 'island01.jpg', 'island02.jpg', null,  null,  null);
+insert into board_common_image(board_num, file1, file2, file3, file4, file5) values(2, 'cartier01.jpg', null, null, null, null);
+
+-- 댓글
+insert into reply(board_num, coment, user_id) values(2, '갤럭시에 애플워치 쓰심??', 'cccc');
+insert into reply(board_num, coment, user_id) values(2, '고급스럽네요 ㅎㅎㅎ', 'dddd');
+
